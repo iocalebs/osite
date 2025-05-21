9c99ca92631f16492cc6ac7d1201d8cb73203443
Caleb St-Denis <caleb.stdenis@gmail.com>
2025-05-21 14:51:09 -0400

    Add title element

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,22 @@
+import { MetaProvider, Title } from "@solidjs/meta";
 import "./app.css";
 
 export default function App() {
   return (
-    <main>
-      <h1>InfoTech Ottawa</h1>
-      <p>Welcome to InfoTech Ottawa!</p>
-      <p>
-        Our website is currently under construction. Please pardon our
-        appearance as we build our space.
-      </p>
-      <p>
-        For all business inquiries, please contact{" "}
-        <a href="mailto:caleb@infotechottawa.ca">caleb@infotechottawa.ca</a>.
-      </p>
-    </main>
+    <MetaProvider>
+      <Title>InfoTech Ottawa</Title>
+      <main>
+        <h1>InfoTech Ottawa</h1>
+        <p>Welcome to InfoTech Ottawa!</p>
+        <p>
+          Our website is currently under construction. Please pardon our
+          appearance as we build our space.
+        </p>
+        <p>
+          For all business inquiries, please contact{" "}
+          <a href="mailto:caleb@infotechottawa.ca">caleb@infotechottawa.ca</a>.
+        </p>
+      </main>
+    </MetaProvider>
   );
 }
