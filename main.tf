ae6927b769927f4e254b5611eac0e50605f05c58
Caleb St-Denis <iocalebst@gmail.com>
2025-06-02 08:59:14 -0400

    Switch to Vercel

--- main.tf
+++ main.tf
@@ -4,16 +4,30 @@ terraform {
             source  = "integrations/github"
             version = "~> 6.0"
         }
+        vercel = {
+            source  = "vercel/vercel"
+            version = "~> 3.0"
+        }
     }
 }
 
 provider "github" {
-    owner = "infotecho"
+    // Would prefer to host under the infotecho organization
+    // but I'm not prepared to pay for Vercel Pro just yet
+    owner = "iocalebs"
 }
 
-resource "github_repository" "public_repo" {
+resource "github_repository" "osite" {
     name        = "osite"
     description = "O-Site: InfoTech Ottawa website"
     has_issues = true
     visibility  = "public"
+}
+
+resource "vercel_project" "osite" {
+  name = "osite"
+  git_repository = {
+    type = "github"
+    repo = github_repository.osite.full_name
+  }
 }
