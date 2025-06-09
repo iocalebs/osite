2fb5ee4f423a9fedb687ef03312875b05ac880a6
Caleb St-Denis <iocalebst@gmail.com>
2025-06-09 13:46:18 -0400

    Disable Sentry telemetry for local dev

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -4,14 +4,16 @@ import { solidRouterBrowserTracingIntegration } from "@sentry/solidstart/solidro
 import { mount, StartClient } from "@solidjs/start/client";
 import { injectSpeedInsights } from "@vercel/speed-insights";
 
-Sentry.init({
-  dsn: "https://d1ed2501f04fff038d3e61e2794fb20e@o4509412081467392.ingest.us.sentry.io/4509435304607744",
-  integrations: [
-    Sentry.captureConsoleIntegration(),
-    solidRouterBrowserTracingIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-});
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: "https://d1ed2501f04fff038d3e61e2794fb20e@o4509412081467392.ingest.us.sentry.io/4509435304607744",
+    integrations: [
+      Sentry.captureConsoleIntegration(),
+      solidRouterBrowserTracingIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
 
 injectSpeedInsights();
 
